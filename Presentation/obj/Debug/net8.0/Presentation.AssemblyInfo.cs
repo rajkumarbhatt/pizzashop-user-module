5b0714983a5077b659976e50165d5751b7e2a9d2
Rajkumar Bhatt <tatvasoft.pci88@outlook.com>
2025-02-28 19:05:42 +0530

    working on roles and permissions tab, changing ui left, rest is done

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88cc84fe29a4689ae5a3d8794b100286cbbc025")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc4d2641a616e4e978058401729b80bd4623d14f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
